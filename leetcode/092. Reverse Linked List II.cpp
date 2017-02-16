121db5059480f0a7e45e5da81296f7dc711f578e
ZJ.Kang <zhengjian.kang@nyu.edu>
2017-02-15 21:31:16 -0500

    092. Reverse linked list II

---
+++ leetcode/092. Reverse Linked List II.cpp
@@ -0,0 +1,50 @@
+/*
+ Author:     Zhengjian Kang
+ Email:      zhengjian.kang@nyu.edu
+ Problem:    Reverse Linked List II
+ Source:     https://leetcode.com/problems/reverse-linked-list-ii/
+ Difficulty: Medium
+ Tags:       {Linked List}
+ 
+ Notes:
+ There is no Reverse Linked List I.
+ Reverse a linked list from position m to n. Do it in-place and in one-pass.
+ For example:
+ Given 1->2->3->4->5->NULL, m = 2 and n = 4,
+ return 1->4->3->2->5->NULL.
+ Note:
+ Given m, n satisfy the following condition:
+ 1 <= m <= n <= length of list.
+
+ Solution: in-place & one-pass.
+ */
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+	ListNode *reverseBetween(ListNode *head, int m, int n) {
+		ListNode dummy(0);
+		dummy.next = head;
+		head = &dummy;   
+		for (int i = 0; i < m - 1; ++i) {
+			head = head->next;
+		}
+		ListNode *cur = head->next;
+		for (int i = 0; i < n - m; ++i) {
+			ListNode *move = cur->next;
+			cur->next = move->next;
+			move->next = head->next;
+			head->next = move;
+		}
+		return dummy.next;
+	}
+};
+
+
