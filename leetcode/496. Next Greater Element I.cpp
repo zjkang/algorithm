44331f4c8a67272880c7272c83f4a76b2630d660
ZJ.Kang <zhengjian.kang@nyu.edu>
2017-06-18 23:11:49 -0400

    496. Next Greater Element I

--- leetcode/496. Next Greater Element I.cpp
+++ leetcode/496. Next Greater Element I.cpp
@@ -21,6 +21,7 @@
      For number 4 in the first array, you cannot find the next greater number for it in the second array, so output -1.
      For number 1 in the first array, the next greater number for it in the second array is 3.
      For number 2 in the first array, there is no next greater number for it in the second array, so output -1.
+ 
  Example 2:
  Input: nums1 = [2,4], nums2 = [1,2,3,4].
  Output: [3,-1]
@@ -40,7 +41,7 @@ public:
         stack<int> s;
         unordered_map<int, int> map; // <val, next greater val>
         for (int n : nums) {
-            while (!s.empty() && s.top() < n) {
+            while (!s.empty() && s.top() < n) { // stack to add next greater val
                 map[s.top()] = n;
                 s.pop();
             }
