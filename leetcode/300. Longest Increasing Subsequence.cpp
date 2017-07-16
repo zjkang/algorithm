f0ddcdccc9d7b4f6d46f61243e332f4f5b97b40e
ZJ.Kang <zhengjian.kang@nyu.edu>
2017-07-16 11:04:16 -0400

    300. Longest Increasing Subsequence

--- leetcode/300. Longest Increasing Subsequence.cpp
+++ leetcode/300. Longest Increasing Subsequence.cpp
@@ -11,7 +11,8 @@
 
  For example,
  Given [10, 9, 2, 5, 3, 7, 101, 18],
- The longest increasing subsequence is [2, 3, 7, 101], therefore the length is 4. Note that there may be more than one LIS combination, it is only necessary for you to return the length.
+ The longest increasing subsequence is [2, 3, 7, 101], therefore the length is 4. 
+ Note that there may be more than one LIS combination, it is only necessary for you to return the length.
 
  Your algorithm should run in O(n2) complexity.
 
@@ -36,6 +37,7 @@ public:
     }
     // O(n log n) 
     int lengthOfLIS(vector<int>& nums) {
+        // refer https://leetcode.com/problems/longest-increasing-subsequence/#/solution
         vector<int> res;
         for(int i=0; i<nums.size(); i++) {
             auto it = std::lower_bound(res.begin(), res.end(), nums[i]);
