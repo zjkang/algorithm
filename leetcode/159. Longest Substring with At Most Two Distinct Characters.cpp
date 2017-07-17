a9d3825ec5ac3f573d558c01daaff3483610ed25
ZJ.Kang <zhengjian.kang@nyu.edu>
2017-07-16 22:51:13 -0400

    159. Longest Substring with At Most Two Distinct Characters

---
+++ leetcode/159. Longest Substring with At Most Two Distinct Characters.cpp
@@ -0,0 +1,39 @@
+/*
+ Author:     Zhengjian Kang
+ Email:      zhengjian.kang@nyu.edu
+ Problem:    Longest Substring with At Most Two Distinct Characters 
+ Source:     https://leetcode.com/problems/longest-substring-with-at-most-two-distinct-characters/#/description
+ Difficulty: Hard
+ Topic:      Hash Table, Two Pointers, String
+ Company:    Google
+
+ Notes:
+ Given a string, find the length of the longest substring T that contains at most 2 distinct characters.
+ For example, Given s = “eceba”,
+ T is "ece" which its length is 3.
+*/
+
+class Solution {
+    int lengthOfLongestSubstringTwoDistinct(string s) {
+        int res = 0, count = 0;
+        vector<int> need(256, 0);
+        int i = 0, j = 0;
+        while (j < s.size()) {
+            if (need[s[j]] == 0) count++;
+            need[s[j]]++;
+            if (count <= 2) {
+                j++; 
+                continue;
+            }
+            res = max(res, j - i);
+            while (i < j && count == 3) {
+                need[s[i]]--;
+                if (need[s[i]] == 0) count--;
+                i++;
+            }
+            j++;
+        }
+        res = max(res, j - i);
+        return res;
+    }
+}
