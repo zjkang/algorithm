e8b27687c7b2cbda4d0fc148230aa5a1676e62e1
zjkang <zkang13@c02r33n1fvh7.fios-router.home>
2017-09-05 22:10:07 -0400

    Update 167. Two Sum II

--- leetcode/167. Two Sum II - Input array is sorted.cpp
+++ leetcode/167. Two Sum II - Input array is sorted.cpp
@@ -1,41 +1,61 @@
 /*
  Author:     Zhengjian Kang
- Email:      zhengjian.kang@nyu.edu
+ Email:      zhengjian.kang@nyu.edu 
  Problem:    Two Sum II - Input array is sorted
- Source:     https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/?tab=Description
- Difficulty: Easy
- Tags:       {Array}, {Two Pointers}, {Binary Search}
- Company:    Amazon
-
- Notes:
+ Source:     https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/description/
+ 
+ Notes:  
  Given an array of integers that is already sorted in ascending order, 
  find two numbers such that they add up to a specific target number.
-
  The function twoSum should return indices of the two numbers such that they add up to the target, 
  where index1 must be less than index2. 
  Please note that your returned answers (both index1 and index2) are not zero-based.
 
- You may assume that each input would have exactly one solution and you may not use the same element twice.
+ You may assume that each input would have exactly one solution.
 
  Input: numbers={2, 7, 11, 15}, target=9
- Output: index1=1, index2=2 
+ Output: index1=1, index2=2
+
+ Solution: Two Pointers Binary Search
 */
 
+// 20ms
 class Solution {
 public:
-    vector<int> twoSum(vector<int> &numbers, int target) {
-        int i = 0, j = numbers.size() - 1;
-        while (i < j) {
-            int sum = numbers[i] + numbers[j];
-            if (sum == target) {
-                vector<int> res = {i+1, j+1};
-                return res;
-            } else if (sum < target) {
-                i++;
-            } else {
-                j--;
-            }
-        }
-        return {-1, -1};
-    }
+	vector<int> twoSum(vector<int> &numbers, int target) {
+		int l = 0, r = numbers.size()-1;
+		while (l < r) {
+			int sum = numbers[l] + numbers[r];
+			if (sum == target) break;
+			else if (sum < target) l++;
+			else r--;
+		}
+		return {l + 1, r + 1};
+	}
+};
+
+
+// 13ms
+class Solution {
+public:
+	vector<int> twoSum(vector<int> &numbers, int target) {
+		for (int i = 0; i < numbers.size()-1; ++i) {
+			int idx = binSearch(numbers, target - numbers[i], i);
+			if (idx != -1) {
+				return {i+1, idx+1};
+			}
+		}    
+		return {-1, -1};
+	}
+
+	int binSearch(vector<int> &numbers, int target, int start) {
+		int l = start + 1, r = numbers.size() - 1;
+		while (l <= r) {
+			int m = (l + r) / 2;
+			if (target == numbers[m]) return m;
+			else if (target < numbers[m]) r = m - 1;
+			else l = m + 1;
+		}
+		return -1;
+	}
 };
